e81800af67c57fdeac10555110bc557e51612861
Filip Teply <fteply.ft@gmail.com>
2018-12-12 18:08:00 +0100

    EntityFramework downloaded and Context

---
+++ GiTinder/Data/GiTinderContext.cs
@@ -0,0 +1,31 @@
+﻿using GiTinder.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GiTinder.Models
+{
+    public class GiTinderContext : DbContext
+    {
+        public DbSet<Settings> Settings { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseMySQL("server=localhost;database=VGBlibrary;user=root;password=1234");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+        //public GiTinderContext(DbContextOptions<GiTinderContext> options)
+        //    : base(options)
+        //{
+
+        //}
+
+
+
+
+    }
+}
---
+++ GiTinder/Models/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiTinder.Models
+{
+    public class Settings
+    {
+
+        public string UserName { get; set; }
+        //        Entity is created belonging to a user
+        //User model updated
+        //Repository is created
+        //Database migration is created
+        //Only valid settings can be saved in the database
+        //Test for checking validity of the settings
+
+        public Settings()
+        {
+
+        }
+
+    }
+}

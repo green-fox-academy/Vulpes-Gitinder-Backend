bc458336f07be6ae1f169e076a89e42a863b3e53
Jonathan Bonnin <bonninj@gmail.com>
2018-12-12 16:13:08 +0100

    create GiTinderContext with DbSet<User>

---
+++ GiTinder/Data/GiTinderContext.cs
@@ -0,0 +1,10 @@
+﻿using GiTinder.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GiTinder.Data
+{
+    public class GiTinderContext : DbContext 
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}

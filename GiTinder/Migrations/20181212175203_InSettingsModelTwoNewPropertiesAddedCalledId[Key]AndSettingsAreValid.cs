aeaf32ddc095539d6aeeb3951ab21e6590943290
Filip Teply <fteply.ft@gmail.com>
2018-12-12 18:54:19 +0100

    add-migration InSettingsModelTwoNewPropertiesAddedCalledId[Key]AndSettingsAreValid

---
+++ GiTinder/Migrations/20181212175203_InSettingsModelTwoNewPropertiesAddedCalledId[Key]AndSettingsAreValid.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GiTinder.Migrations
+{
+    public partial class InSettingsModelTwoNewPropertiesAddedCalledIdKeyAndSettingsAreValid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    UserName = table.Column<string>(nullable: true),
+                    SettingsAreValid = table.Column<short>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Settings");
+        }
+    }
+}
--- GiTinder/Models/Settings.cs
+++ GiTinder/Models/Settings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace GiTinder.Models
 {
     public class Settings
     {
-
+        [Key]
+        public int Id { get; set; }
         public string UserName { get; set; }
+        public bool SettingsAreValid { get; set; }
+           
         //        Entity is created belonging to a user
         //User model updated
         //Repository is created

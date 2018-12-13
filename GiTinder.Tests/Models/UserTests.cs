1aea30d120b73dc2df3349a2fbeae5ea30cd41b8
Jonathan Bonnin <bonninj@gmail.com>
2018-12-13 12:53:21 +0100

    add first UserTest

--- GiTinder.Tests/Models/UserTests.cs
+++ GiTinder.Tests/Models/UserTests.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using Xunit;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using GiTinder.Models;
 
 namespace GiTinder.Tests.Models
 {
-    class UserTests
+    public class UserTests
     {
+        [Fact]
+        public void CanCreateUser()
+        {
+            User user = new User();
+            Assert.True(user != null);
+        }
     }
 }

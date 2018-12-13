51bfb713bd1c93f65a0739e7f11002567cf74d40
Jonathan Bonnin <bonninj@gmail.com>
2018-12-13 11:23:55 +0100

    rm fields in User, validate props and change Key

--- GiTinder/Models/User.cs
+++ GiTinder/Models/User.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace GiTinder.Models
 {
     public class User
     {
-        private int id;
-        private string userName;
-        private string userToken;
-        private int reposCount;
-
-        public int Id { get => id; set => id = value; }
-        public string UserName { get => userName; set => userName = value; }
-        public int ReposCount { get => reposCount; set => reposCount = value; }
-        public string UserToken { get => userToken; set => userToken = value; }
+        
+        [Key, Required]
+        public string UserName { get; set; }
+        public int ReposCount { get; set; }
+        [Required]
+        public string UserToken { get; set; }
     }
 }

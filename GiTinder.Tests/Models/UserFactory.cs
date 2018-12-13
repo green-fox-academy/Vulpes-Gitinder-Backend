d1a688e476de2539844f0db3625a79b960e1ccd5
Jonathan Bonnin <bonninj@gmail.com>
2018-12-13 18:21:40 +0100

    add UserFactory for testing purposes

---
+++ GiTinder.Tests/Models/UserFactory.cs
@@ -0,0 +1,27 @@
+﻿using GiTinder.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GiTinder.Tests.Models
+{
+    class UserFactory
+    {
+        public static User CreateUser()
+        {
+            return new User()
+            {
+                UserName = "Michel",
+                ReposCount = 8,
+                UserToken = "abc123"
+            };
+        }
+
+        public static User CreateUserWithNullUserName()
+        {
+            var user = CreateUser();
+            user.UserName = null;
+            return user;
+        }
+    }
+}

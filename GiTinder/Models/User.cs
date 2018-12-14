e1c6ac151e4b5dcea6d8a86d01cb2bf7e12becec
Jonathan <bonninj@gmail.com>
2018-12-14 12:12:31 +0100

    Jonathan (#3)
    * create user class
    
    * create GiTinderContext with DbSet<User>
    
    * add migration files for User
    
    * rm fields in User, validate props and change Key
    
    * add user validation and changed primary key
    
    * create GiTinder.Tests project with UserTests.cs
    
    * add first UserTest
    
    * add test CanSetUserNameToString
    
    * edit conditional prop assignment
    
    * add UserFactory for testing purposes
    
    * add tests for validation of the user

---
+++ GiTinder/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GiTinder.Models
+{
+    public class User
+    {
+        
+        [Key]
+        [Required]
+        [MinLength(1)]
+        public string UserName { get; set; }
+        public int ReposCount { get; set; }
+        [Required]
+        public string UserToken { get; set; }
+    }
+}

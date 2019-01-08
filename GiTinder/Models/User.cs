aa27619e7f518e58a33132b0518aae8e30df35c7
Tomas <tmoruszewicz@gmail.com>
2019-01-08 17:48:06 +0100

    User changed

--- GiTinder/Models/User.cs
+++ GiTinder/Models/User.cs
@@ -2,19 +2,19 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-=======
-﻿using System.ComponentModel.DataAnnotations;
->>>>>>> b8a52ca500712c4e2ec5919c74bb702fa02b19ef
 
 namespace GiTinder.Models
 {
     public class User
     {
-<<<<<<< HEAD
         public static Task Content { get; internal set; }
         public int Id { get; set; }
+        [Key]
+        [Required]
+        [MinLength(1)]
         [JsonProperty("login")]
         public string UserName { get; set; }
         [JsonProperty("public_repos")]
@@ -24,19 +24,5 @@ namespace GiTinder.Models
         [JsonProperty("avatar_url")]
         public string Avatar { get; set; }
         public string Repos { get; set; }
-        //public List<string> Languages { get; set; }
-        //public List<string> Snippets { get; set; }
     }
 }
-=======
-        
-        [Key]
-        [Required]
-        [MinLength(1)]
-        public string UserName { get; set; }
-        public int ReposCount { get; set; }
-        [Required]
-        public string UserToken { get; set; }
-    }
-}
->>>>>>> b8a52ca500712c4e2ec5919c74bb702fa02b19ef

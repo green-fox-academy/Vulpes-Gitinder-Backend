81b43014a8791797b39bd4e01cf9263384c29a1f
Filip Teply <fteply.ft@gmail.com>
2019-01-08 23:13:26 +0100

    SettingsLanguage entity working, get settings controller including preferred_languages is functional, structure of settings DAO json object modified

--- GiTinder/Models/SettingsDAO.cs
+++ GiTinder/Models/SettingsDAO.cs
@@ -7,8 +7,17 @@ namespace GiTinder.Models
 {
     public class SettingsDAO
     {
-
+        [JsonIgnore]
         public Settings settingsForDAO;
+        
+        [JsonProperty("enable_notifications")]
+        public bool EnableNotification { get; set; }
+
+        [JsonProperty("enable_background_sync")]
+        public bool EnableBackgroundSync { get; set; }
+
+        [JsonProperty("max_distance")]
+        public int MaxDistanceInKm { get; set; }
 
         [JsonProperty("preferred_languages")]
         public List<string> PreferredLanguagesNames { get; set; }
@@ -16,50 +25,13 @@ namespace GiTinder.Models
         public SettingsDAO(Settings settings)
         {
             settingsForDAO = settings;
+            EnableNotification = settingsForDAO.EnableNotification;
+            EnableBackgroundSync = settingsForDAO.EnableBackgroundSync;
+            MaxDistanceInKm = settingsForDAO.MaxDistanceInKm;
 
-           // var settingsId = settings.SettingsId;
-            
             List<string> preferredLanguagesNames = new List<string>();
-
-            // var foundSettings = _context.Settings.Include(e => e.SettingsLanguages).ThenInclude(l => l.Language).Where(s => s.UserName == foundUser.UserName).FirstOrDefault();
-
-            //        var result = context.Begrip
-            //.Include(x => x.Categories)
-            //.ThenInclude(x => x.category);
-
             preferredLanguagesNames = settings.SettingsLanguages.Select(sl => sl.Language.LanguageName).ToList();
-
             PreferredLanguagesNames = preferredLanguagesNames;
-
-            //  List<string> firstNames = people.Select(person => person.FirstName).ToList();
-
-            //List<object> objects = new List<object>();
-            //List<string> strings = objects.Select(s => (string)s).ToList();
-
-            //.Where(sl => sl.SettingsId == settings.SettingsId)
-
-
-            //            .SelectMany(p => p.PersonClubs);
-
-            //        .Where(p => p.PersonId == id)
-            //        .SelectMany(p => p.PersonClubs);
-            //.Select(pc => pc.Club);
-
-            //        var sllist  = settings.SettingsLanguages.Where(sl => sl.SettingsId == settings.SettingsId);
-            //            var landIdlist =
-            //        preferredLanguagesNames 
-            // Select(s => s.Language).LanguageName.ToList();
-
-            //preferredLanguagesNames = settings.SettingsLanguages
-            //    //.Where(s => s.Settings == settings)
-            //    //.Include(e => e.SettingsLanguages)
-            //    //.Include(l => l.Language)
-            //   .Select(s => s.settingsId)
-            //    .ToList();
-
-            //preferredLanguagesNames = settings.PreferredLanguagesList.Select(lang => lang.LanguageName).ToList();
-
-
         }
     }
 }

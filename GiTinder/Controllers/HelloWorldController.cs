d8ff106a3a9dc3543deb12dbc601bc38a7dcb1ca
Jonathan Bonnin <bonninj@gmail.com>
2019-01-09 12:57:00 +0100

    clean default endpoints and unneeded 'using' statement

---
+++ GiTinder/Controllers/HelloWorldController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GiTinder.Controllers
+{
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet("/hello")]
+        public ActionResult<string> SayHello()
+        {
+            return "Hello, World! I am an endpoint that returns this string and does nothing else";
+        }
+    }
+}

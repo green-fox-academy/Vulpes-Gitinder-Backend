ee60b5bc2f8b605ad5ee2901399b33b78ef73555
Jonathan Bonnin <bonninj@gmail.com>
2019-01-10 18:15:49 +0100

    add /hello test

---
+++ GiTinder.Tests/Controllers/HelloWorldControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using GiTinder.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GiTinder.Tests.Controllers
+{
+    public class HelloWorldControllerTest
+    {
+        [Fact]
+        public void HelloReturnsExpectedString()
+        { 
+            var helloWorldController = new HelloWorldController();
+            var expectedResult = "Hello, World! I am an endpoint that returns this string and does nothing else";
+            var actualResult = helloWorldController.SayHello().Value;
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+    }
+}

f89f7615c3ba650ff1a8ea51f4e59705bd8913a7
Tomas <tmoruszewicz@gmail.com>
2019-01-18 17:06:54 +0100

    Created swipeController class

---
+++ GiTinder.Tests/Controllers/SwipeControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using GiTinder.Controllers;
+using GiTinder.Data;
+using GiTinder.Services;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GiTinder.Tests.Controllers
+{
+    Mock<GiTinderContext> mockRepo;
+    UserServices userServices;
+    UsersController usersController;
+    HeaderDictionary headerDictionary;
+    Mock<HttpResponse> response;
+    Mock<HttpContext> httpContext;
+
+    class SwipeControllerTest
+    {
+    }
+}

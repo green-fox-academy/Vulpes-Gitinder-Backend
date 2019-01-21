4c7dd6cfe99aa9bd34b97485a47a068d5e464708
Jonathan Bonnin <bonninj@gmail.com>
2019-01-21 17:18:44 +0100

    create availableResponseBody.cs

---
+++ GiTinder/Models/Responses/AvailableResponseBody.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiTinder.Models.Responses
+{
+    public class AvailableResponseBody
+    {
+        [JsonProperty("profiles")]
+        public List<Profile> Profiles { get; set; }
+        [JsonProperty("count")]
+        public int Count { get; set; }
+        [JsonProperty("all")]
+        public int All { get; set; }
+    }
+}

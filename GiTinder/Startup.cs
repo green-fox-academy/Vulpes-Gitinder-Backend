cf32d2bc584ca22bbcb2d8706fc74ddb88b5ea3f
Pavel Cicu <cicupa@gmail.com>
2019-01-23 15:48:33 +0100

    setting up the database, connection string

--- GiTinder/Startup.cs
+++ GiTinder/Startup.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using GiTinder.Data;
 using GiTinder.Services;
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -46,8 +45,7 @@ namespace GiTinder
             {
                 app.UseHsts();
             }        
-            //app.UseDefaultFiles();
-
+            
             app.UseHttpsRedirection();
             app.UseMvc();
             using (var serviceScope = app.ApplicationServices.CreateScope())

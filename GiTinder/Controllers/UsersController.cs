c2a7b6d484b67f6868c906ed7dfed8116ea047cf
Pavel Cicu <cicupa@gmail.com>
2019-01-23 19:59:10 +0100

    endpoint DELETE /logout created

--- GiTinder/Controllers/UsersController.cs
+++ GiTinder/Controllers/UsersController.cs
@@ -70,6 +70,31 @@ namespace GiTinder.Controllers
 
         }
 
+        [HttpDelete("/logout")]
+        public object Logout()
+        {
+            var usertoken = Request.Headers["X-Gitinder-Token"];
+            GeneralApiResponseBody responseBody;
+            //var responseProfile = _userServices.FindUserByUserToken(usertoken);
+
+            if (string.IsNullOrEmpty(usertoken))
+            {
+                Response.StatusCode = 403;
+                responseBody = new ErrorResponseBody("Unauthorized request!");
+            }
+            else if (_userServices.TokenExists(usertoken))
+            {
+                responseBody = new OKResponseBody("Logged out successfully!");
+            }
+            else
+            {
+                Response.StatusCode = 403;
+                responseBody = new ErrorResponseBody("Unauthorized request!");
+            }
+            return responseBody;
+        }
+
+
     }
 }
 

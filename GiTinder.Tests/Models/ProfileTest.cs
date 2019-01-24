b795fc82a98bf9af2460b2e604b657ebff15b31f
Pavel Cicu <cicupa@gmail.com>
2019-01-24 11:41:39 +0100

    start with testing

---
+++ GiTinder.Tests/Models/ProfileTest.cs
@@ -0,0 +1,34 @@
+﻿using GiTinder.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Xunit;
+
+namespace GiTinder.Tests.Models
+{
+    public class ProfileTest
+    {
+        [Fact]
+        public void IsItTokenNull()
+        {
+            var usertoken = ProfileFactory.CreateTokenWithNullValue();
+            Assert.False(ValidateModel(usertoken).Count == 0);
+        }
+        [Fact]
+        public void CanSetTokenWithString()
+        {
+            var usertoken = new Profile("abc123");
+            Assert.Equal("abc123",usertoken.UserToken);
+        }
+
+
+        private IList<ValidationResult> ValidateModel(object model)
+        {
+            var validationResults = new List<ValidationResult>();
+            var ctx = new ValidationContext(model, null, null);
+            Validator.TryValidateObject(model, ctx, validationResults, true);
+            return validationResults;
+        }
+    }
+}
---
+++ GiTinder/Models/Profile.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiTinder.Models
+{
+    public class Profile
+    {
+        [Key]
+        [Required]
+        [MinLength(1)]      
+        [JsonProperty("usertoken")]
+        public string UserToken { get; set; }
+
+        public Profile()
+        {
+
+        }
+
+        public Profile(string usertoken)
+        {
+            this.UserToken = usertoken;
+        }
+    }
+}

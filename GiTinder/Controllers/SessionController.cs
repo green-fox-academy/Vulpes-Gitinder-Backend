de46805fed6198eeade736e08e8d0843851101dc
Pavel Cicu <cicupa@gmail.com>
2019-01-29 13:12:40 +0100

    logout endpoint changed to proper version

--- GiTinder/Controllers/SessionController.cs
+++ GiTinder/Controllers/SessionController.cs
@@ -50,25 +50,26 @@ namespace GiTinder.Controllers
         }
 
         [HttpDelete("/logout")]
-        public GeneralApiResponseBody MockLogout()
+        public object Logout()
         {
-            string Token = Request.Headers["X-Gitinder-Token"];
+            var usertoken = Request.Headers["X-Gitinder-Token"];
             GeneralApiResponseBody responseBody;
+            var responseUser = _userServices.FindUserByUserToken(usertoken);
 
-            if (Token == null)
+            if (string.IsNullOrEmpty(usertoken))
             {
                 Response.StatusCode = 403;
-                responseBody = new ErrorResponseBody("X-Gititnder-token is missing!");
+                responseBody = new ErrorResponseBody("Unauthorized request!");
             }
-            else if (Token == "abc")
+            else if (_userServices.TokenExists(usertoken))
             {
                 responseBody = new OKResponseBody("Logged out successfully!");
+                _userServices.RemoveToken(usertoken, responseUser.Username);
             }
             else
             {
                 Response.StatusCode = 403;
-                responseBody = new ErrorResponseBody("bad X-Gititnder-token");
-
+                responseBody = new ErrorResponseBody("Unauthorized request!");
             }
             return responseBody;
         }

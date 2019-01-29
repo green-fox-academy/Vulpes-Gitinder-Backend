7d6cbf141bff59113a4a8ad599b1fce6ad836aed
Filip Teply <fteply.ft@gmail.com>
2019-01-29 14:32:18 +0100

    initial change in UserController

--- GiTinder/Controllers/UsersController.cs
+++ GiTinder/Controllers/UsersController.cs
@@ -21,7 +21,7 @@ namespace GiTinder.Controllers
             _context = context;
             _userServices = userServices;
         }
-               
+
 
         [HttpGet("/profile")]
         public GeneralApiResponseBody MockProfile()
@@ -41,19 +41,25 @@ namespace GiTinder.Controllers
             return responseBody;
         }
 
-        [HttpGet("/available/{page?}")]
-        public GeneralApiResponseBody ShowAvailableProfiles()
+        [HttpGet("/available/{page:int?}")]
+        public GeneralApiResponseBody ShowAvailableProfiles(int page = 1)
         {
+            int allProfiles;
+            int countOfProfilesOnPage1;
+            int numberOfPages;
             GeneralApiResponseBody responseBody;
             string token = Request.Headers["X-Gitinder-Token"];
 
-            if (string.IsNullOrEmpty(token))
+            if (string.IsNullOrEmpty(token) || !_userServices.TokenExists(token))
             {
                 Response.StatusCode = 403;
                 responseBody = new ErrorResponseBody("Unauthorized request!");
             }
+
             else
             {
+                //ProfileResponse(string username, string avatar, string repos, string languages)
+
                 string adamImage = "https://f22bfca7a5abd176cefa-59c40a19620c1f22577ade10e9206cf5.ssl.cf1.rackcdn.com/571x670/sir-adam-mbo-k-01-x2-1.jpg";
                 string michelImage = "http://ichef-1.bbci.co.uk/news/304/media/images/63133000/jpg/_63133978_francoishollande.jpg";
                 string pavelImage = "https://inpolitics.ro/wp-content/uploads/2014/04/wpid-pavel-abraham6-280x222.jpg";
@@ -67,9 +73,53 @@ namespace GiTinder.Controllers
                     new ProfileResponse("Tomek Snake", tomekImage, "my-first-repo", "Python"),
                     new ProfileResponse("Filip-The-Chemist", filipImage, "blue-meth-for-dummies", "C"),
                 };
-                responseBody = new AvailableResponseBody(profiles, 5, 5);
+
+                allProfiles = profiles.Count();
+                double allProfilesAsDouble = Convert.ToDouble(allProfiles);
+                numberOfPages = (int)Math.Ceiling(Convert.ToDouble(allProfiles) / 20.00);
+                
+                if (allProfiles < 20)
+                {
+                    countOfProfilesOnPage1 = allProfiles;
+                }
+                else
+                {
+                    countOfProfilesOnPage1 = 20;
+                }
+
+                responseBody = new AvailableResponseBody(profiles, countOfProfilesOnPage1, allProfiles);
             }
             return responseBody;
         }
     }
 }
+
+//public class ProfileResponse : GeneralApiResponseBody
+//{
+//    [JsonProperty("username")]
+//    public string Username { get; set; }
+//    [JsonProperty("url")]
+//    public string Avatar { get; set; }
+//    [JsonProperty("repos")]
+//    public string Repos { get; set; }
+//    [JsonProperty("languages")]
+//    public string Languages { get; set; }
+
+
+//    public ProfileResponse()
+//    {
+//        Username = "aze";
+//        Avatar = "https://avatars1.githubusercontent.com/u/5855091?s=400&v=4";
+//        Repos = "string";
+//        Languages = "eng";
+
+//    }
+
+//    public ProfileResponse(string username, string avatar, string repos, string languages)
+//    {
+//        Username = username;
+//        Avatar = avatar;
+//        Repos = repos;
+//        Languages = languages;
+//    }
+//}

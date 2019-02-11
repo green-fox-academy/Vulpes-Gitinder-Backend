2b08a90d3d4efff9423d0763061c7b0081aeabb4
Jonathan Bonnin <bonninj@gmail.com>
2019-02-11 15:12:07 +0100

    create message entity

---
+++ GiTinder/Models/Message.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiTinder.Models
+{
+    public class Message
+    {
+        [Key]
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("from")]
+        public string From { get; set; }
+        [JsonProperty("to")]
+        public string To { get; set; }
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+        [JsonProperty("message")]
+        public string Content { get; set; }
+    }
+}

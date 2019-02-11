a8cbca68c6d7901110911ea2d4a833d4b3eccf8b
Jonathan Bonnin <bonninj@gmail.com>
2019-02-11 17:17:05 +0100

    add MessageTests and MessageFactory

---
+++ GiTinder.Tests/Models/MessageFactory.cs
@@ -0,0 +1,42 @@
+﻿using GiTinder.Models;
+using System;
+
+namespace GiTinder.Tests.Models
+{
+    class MessageFactory
+    {
+        public static Message CreateValidMessage()
+        {
+            return new Message()
+            {
+                Id = 1,
+                From = "Michel",
+                To = "Raspoutine",
+                CreatedAt = DateTime.Now,
+                Content = "message"                
+            };
+        }
+
+        public static Message MessageWithoutFrom()
+        {
+            var message = CreateValidMessage();
+            message.From = null;
+            return message;
+        }
+
+        public static Message MessageWithoutTo()
+        {
+            var message = CreateValidMessage();
+            message.To = null;
+            return message;
+        }
+
+        public static Message MessageWithoutContent()
+        {
+            var message = CreateValidMessage();
+            message.Content = null;
+            return message;
+        }
+
+    }
+}
